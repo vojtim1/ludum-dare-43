882f314db40006a2818b72e7ae909cc2d23d6f61
vojtim1 <vojtim11@seznam.cz>
2018-12-01 12:28:45 +0100

    Player sprite + Animation added

---
+++ Tough hunt/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            GetComponent<Animator>().CrossFade("Walk", 0);
+            if (!GetComponent<PlayerMotor>().IsGoingRight())
+                GetComponent<SpriteRenderer>().flipX = true;
+            else GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else GetComponent<Animator>().CrossFade("Idle", 0);
+    }
+}
