62208d78c218f76367e2610e2f40dbf0db8b21db
Dominik Vít <slipiklp@gmail.com>
2018-12-01 15:12:36 +0100

    Merging

---
+++ Tough hunt/Assets/Scripts/Game Mechanics/Village.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Village : MonoBehaviour
+{
+
+	public static Village instance;
+
+	private int currentFood;
+	public int dailyFoodIncome;
+	public int foodToSustainRaid;
+
+	public float raidChance;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void Start()
+	{
+		currentFood = 0;
+	}
+
+	public void NewDay()
+	{
+		currentFood -= dailyFoodIncome;
+		if(currentFood < 0)
+		{
+			currentFood = 0;
+		}
+
+		raidChance += 0.1f;
+		GameController.instance.testingText.text = "";
+	}
+
+	public void AddFood(int amount)
+	{
+		currentFood += amount;
+	}
+
+	public void StartRaid(int currentDay)
+	{
+		if(Random.Range(0.0f, 1.0f) > raidChance)
+		{
+			GameController.instance.testingText.text = "escaped";
+			return;
+		}
+		raidChance = 0.1f;
+
+		if (currentFood >= foodToSustainRaid * currentDay)
+		{
+			// OK
+		}
+		else
+		{
+			GameController.instance.GameOver();
+		}
+	}
+}
