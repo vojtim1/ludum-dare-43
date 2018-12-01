ea4238c668dbc558aa1f469b7e3881f01eb9b38f
Eliáš Cizl <cizl.elias@gmail.com>
2018-12-01 15:54:48 +0100

    Great hall colliders

---
+++ Tough hunt/Assets/Scripts/Game Mechanics/SacrificeTheLoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SacrificeTheLoot : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "player")
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				GameController.instance.SacrificeTheLoot();
+			}
+		}
+	}
+}
