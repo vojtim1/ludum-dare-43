92ef37cf30539a0edcacfca9495865a28a547426
Eliáš Cizl <cizl.elias@gmail.com>
2018-12-02 10:35:25 +0100

    Fixed lighting and sleep

---
+++ Tough hunt/Assets/TextDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplay : MonoBehaviour {
+
+	public string toDisplay;
+	public Text textField;
+
+	private void Start()
+	{
+		SetText(0);
+	}
+	public void SetText(int amount)
+	{
+		textField.text = toDisplay + ": " + amount;
+	}
+}
