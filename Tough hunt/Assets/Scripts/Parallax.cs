1a04c2620974221ceeb30d27409289839a3ec6be
Eliáš Cizl <cizl.elias@gmail.com>
2018-12-02 23:33:30 +0100

    Parallax has come

---
+++ Tough hunt/Assets/Scripts/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    Vector2 cameraStartPosition;
+    Vector2 startPosition;
+    Camera mainCamera;
+
+    [SerializeField]
+    float scale = 1;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+        startPosition = transform.position;
+        cameraStartPosition = mainCamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = startPosition + ((Vector2)mainCamera.transform.position - cameraStartPosition) * scale;
+
+	}
+}
