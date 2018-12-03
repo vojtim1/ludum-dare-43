fbe0625b624b41749b48382fc9a0e3dc730edba5
vojtim1 <vojtim11@seznam.cz>
2018-12-03 20:50:28 +0100

    VolumeSliderScript

---
+++ Tough hunt/Assets/VolumeSlider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class VolumeSlider : MonoBehaviour {
+    Slider slider;
+
+    // Use this for initialization
+    void Start() {
+        slider = GetComponent<Slider>();
+        slider.value = Settings.instance.GetVolume();
+        slider.onValueChanged.RemoveAllListeners();
+        slider.onValueChanged.AddListener(delegate { SetVolume(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetVolume()
+    {
+        print("Slider");
+        print(slider.value);
+        Settings.instance.SetVolume(slider.value);
+    }
+}
