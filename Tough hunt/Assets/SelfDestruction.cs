7464d45add94b10d6d231ed62760563bbd804e98
vojtim1 <vojtim11@seznam.cz>
2018-12-03 18:32:53 +0100

    TextBubbleUpdate

---
+++ Tough hunt/Assets/SelfDestruction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SelfDestruction : MonoBehaviour {
+    public float time;
+
+	void Update () {
+        time -= Time.deltaTime;
+        if (time <= 0)
+            Destroy(gameObject);
+	}
+}
