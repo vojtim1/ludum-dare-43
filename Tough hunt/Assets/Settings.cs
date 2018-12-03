e4993659033b69449085d6df2f05af60e9f2bb40
Dominik Vít <slipiklp@gmail.com>
2018-12-03 20:09:42 +0100

    Menu start, logo

---
+++ Tough hunt/Assets/Settings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Settings : MonoBehaviour {
+    public static Settings instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void SetVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+        PlayerPrefs.Save();
+    }
+    public float GetVolume()
+    {
+        return PlayerPrefs.GetFloat("volume");
+    }
+    public void SetShowHints(int showHints)
+    {
+        PlayerPrefs.SetInt("showHints", showHints);
+    }
+    public bool GetShowHints()
+    {
+        if (PlayerPrefs.GetInt("showHints") != 0)
+            return true;
+        else return false;
+    }
+}
