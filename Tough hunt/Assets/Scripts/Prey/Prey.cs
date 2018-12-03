82a19ee5c0019b7240e1036781f33909f7eb0fd4
vojtim1 <vojtim11@seznam.cz>
2018-12-03 22:35:57 +0100

    prey script update

--- Tough hunt/Assets/Scripts/Prey/Prey.cs
+++ Tough hunt/Assets/Scripts/Prey/Prey.cs
@@ -67,7 +67,8 @@ public class Prey : MonoBehaviour {
     {
         if(collision.gameObject.tag == "Player" && !alive)
         {
-            TextBubble.instance.Say("I might skin this animal...", collision.gameObject, 2);
+            if(Settings.instance.GetShowHints())
+                TextBubble.instance.Say("I might skin this animal...", collision.gameObject, 2);
             if (Input.GetKey(KeyCode.E))
             {
                 GameController.instance.AddFood(foodReward);
