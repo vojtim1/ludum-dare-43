8c75e934fec58b98feb63f41eac057c0dc6eb43c
vojtim1 <vojtim11@seznam.cz>
2018-12-04 22:28:28 +0100

    Post LD hotfix - added hours, food indicator fix

---
+++ Tough hunt/Assets/Hours.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hours : MonoBehaviour {
+    public RectTransform yellowArm;
+
+	// Use this for initialization
+	void Start () {
+        yellowArm.rotation = Quaternion.Euler(new Vector3(0,0,90));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float timeRatio = GameController.instance.CurrentGameTime / GameController.instance.TotalDayTime;
+        yellowArm.rotation = Quaternion.Euler(new Vector3(0,0,90+-360*timeRatio));
+	}
+}
